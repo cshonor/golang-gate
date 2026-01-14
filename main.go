7a3d2979869a91f5c7baa605c91bddfdc3d51742
wzp <1239263644@qq.com>
2026-01-14 22:35:27 +0800

    添加中文注释说明代码结构

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
+// 声明本代码所属的包
 package main
 
+// 导入 fmt (是 format 的缩写) 包,使其可用
 import (
 	"fmt"
 )
 
+// 声明一个名为 main 的函数
 func main() {
-	fmt.Println("Hello, 书籍!")
+	// 在屏幕上打印出 "Hello, playground"
+	fmt.Println("Hello, playground")
 }
 
