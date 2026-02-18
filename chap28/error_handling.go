9207c522f3f74806dadb638c5b8782471f079b77
cshonor <53027584+cshonor@users.noreply.github.com>
2026-02-19 01:36:11 +0800

    更新 error_handling.go

--- chap28/error_handling.go
+++ chap28/error_handling.go
@@ -40,6 +40,9 @@ func main() {
 如果你的函数 没写返回 error
  
 那就 不能返回错误，也 不能判断 err。
+- 函数 声明了 error → 才能返回错误、才能判断  err 
+​
+- 函数 没声明 error → 不能返回错误，也拿不到  err
 
 	fmt.Println("特点2：无异常捕获机制")
 	fmt.Println("  Go没有 try/catch")
